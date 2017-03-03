75590a862da92fdcde0d88747e5f22273012ab5d
Toshi7 <ttoshi0719@gmail.com>
2017-03-03 18:48:50 +0900

    Add project files.

---
+++ test/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+using Library;
+
+namespace CharacterCounterTests
+{
+    public class GetCounterForLetter
+    {
+        [Fact]
+        public void Should_return_a_count_of_1_if_there_is_a_single_letter_to_parse() 
+        {
+            CharacterCounter counter = new CharacterCounter();
+            counter.ParseString("a");
+            Assert.Equal(1, counter.GetCountForLetter('a'));
+        }
+    }
+}
