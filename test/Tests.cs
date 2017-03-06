026e3c1b73c168a54cdd7d27c14e30587c636f44
Toshi7 <ttoshi0719@gmail.com>
2017-03-06 18:39:41 +0900

    update the .netcore version

--- test/Tests.cs
+++ test/Tests.cs
@@ -10,8 +10,8 @@ namespace CharacterCounterTests
         public void Should_return_a_count_of_1_if_there_is_a_single_letter_to_parse() 
         {
             CharacterCounter counter = new CharacterCounter();
-            counter.ParseString("a");
-            Assert.Equal(1, counter.GetCountForLetter('a'));
+            counter.ParseString("aabbccaa");
+            Assert.Equal(4, counter.GetCountForLetter('a'));
         }
     }
 }
