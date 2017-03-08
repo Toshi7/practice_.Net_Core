97c333de227ab2cdf2e9b104e6597975a5840e85
Toshi7 <ttoshi0719@gmail.com>
2017-03-08 18:23:24 +0900

    fixed testing code for Test Explorer

--- test/Tests.cs
+++ test/Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Library;
 using Xunit;
+using FluentAssertions;
 
 namespace CharacterCounterTests
 {
@@ -10,8 +11,10 @@ namespace CharacterCounterTests
         public void Should_return_a_count_of_1_if_there_is_a_single_letter_to_parse() 
         {
             CharacterCounter counter = new CharacterCounter();
-            counter.ParseString("aabbccaa");
-            Assert.Equal(4, counter.GetCountForLetter('a'));
+            counter.ParseString("aaadda");
+            counter.GetCountForLetter('a').Should().Be(4);
+            //counter.Equal(4, counter.GetCountForLetter('a'));
+            //            Assert.Equal(4, counter.GetCountForLetter('a'));
         }
     }
 }
